53212c57446bf78d7e0ee000c47b60e00a678906
Gokul Krishna A <gokulkrishna.a2023@vitstudent.ac.in>
2025-10-17 14:43:50 +0530

    Add files via upload

--- Assignment10/q1.php
+++ Assignment10/q1.php
@@ -1,1 +1,26 @@
+<?php
+ob_start();
+
+if (isset($_COOKIE['visits'])) {
+    $count = $_COOKIE['visits'] + 1;
+} else {
+    $count = 1;
+}
+
+setcookie('visits', $count, time() + 3600);
+
+ob_end_flush();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Visit Counter</title>
+</head>
+<body>
+    <h1>Welcome!</h1>
+    <p>You have visited this page <?php echo $count; ?> times.</p>
+</body>
+</html>
 
